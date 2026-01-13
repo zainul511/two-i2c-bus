54db9375d4dca4b032a67736a6bd00e16240bdb6
Zainul <zainulhussain511@gmail.com>
2026-01-13 18:00:15 +0530

    This is the updated code for using two I2C bus method. Here we have updated the code to get rid of garbage value of sensor readings. Also I have added some comments. Here additional I2C scanner is used. it is scanning all possible addresses in a I2C bus.

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -3,46 +3,46 @@
 #include "freertos/task.h"
 #include "driver/i2c.h"
 #include "esp_log.h"
+#include <string.h> // For memset In this code this is used to fill a structutre with zeroes if a sensor reading fails. 
+                    // This prevents printing garbage values when a sensor is disconnected.
 
 // --- Pin Definitions ---
 #define I2C0_SDA_IO     21
 #define I2C0_SCL_IO     22
 #define I2C1_SDA_IO     17
 #define I2C1_SCL_IO     16
-#define I2C_FREQ_HZ     100000
+#define I2C_FREQ_HZ     100000 //Can we increase this to 400kHz later?
 
 // --- MPU6050 Registers ---
-#define MPU6050_PWR_MGMT_1   0x6B
-#define MPU6050_ACCEL_XOUT_H 0x3B  // Start of data block
+#define MPU6050_PWR_MGMT_1   0x6B //This register is used to wake up the MPU6050 from sleep mode.We have to write 0 to it.Then internal clock enables.
+#define MPU6050_ACCEL_XOUT_H 0x3B  //Starting register for accelerometer data(X axis acceleration high byte)
 
 static const char *TAG = "MPU_TRIO";
 
+// --- Data Structure for MPU6050 Readings ---. This is a structure to hold all the sensor data read from the MPU6050 in one go.
 typedef struct {
-    int16_t acc_x;
-    int16_t acc_y;
-    int16_t acc_z;
-    int16_t temp; // Temperature is between Accel and Gyro in memory
-    int16_t gyro_x;
-    int16_t gyro_y;
-    int16_t gyro_z;
-} mpu_data_t;
+    int16_t acc_x; int16_t acc_y; int16_t acc_z;
+    int16_t temp;
+    int16_t gyro_x; int16_t gyro_y; int16_t gyro_z;
+} mpu_data_t; //This is the name of the structure.
 
-// Initialize both I2C Ports
+// --- I2C Initialization (Same as before) ---
+
 void init_i2c_buses() {
-    // --- Config Bus 0 ---
+    // I2C0 Initialization
     i2c_config_t conf0 = {
         .mode = I2C_MODE_MASTER,
         .sda_io_num = I2C0_SDA_IO,
         .scl_io_num = I2C0_SCL_IO,
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = I2C_FREQ_HZ,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,// Enable internal pull-ups
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,// Enable internal pull-ups
+        .master.clk_speed = I2C_FREQ_HZ,// Set clock speed.Hope to 400kHz later
     };
-    i2c_param_config(I2C_NUM_0, &conf0);
-    i2c_driver_install(I2C_NUM_0, conf0.mode, 0, 0, 0);
+    i2c_param_config(I2C_NUM_0, &conf0); // Configure I2C0 with the settings in conf0
+    i2c_driver_install(I2C_NUM_0, conf0.mode, 0, 0, 0); // Install I2C0 driver.
 
-    // --- Config Bus 1 ---
     i2c_config_t conf1 = {
+        // I2C1 Initialization
         .mode = I2C_MODE_MASTER,
         .sda_io_num = I2C1_SDA_IO,
         .scl_io_num = I2C1_SCL_IO,
@@ -51,64 +51,89 @@ void init_i2c_buses() {
         .master.clk_speed = I2C_FREQ_HZ,
     };
     i2c_param_config(I2C_NUM_1, &conf1);
-    i2c_driver_install(I2C_NUM_1, conf1.mode, 0, 0, 0);
+    i2c_driver_install(I2C_NUM_1, conf1.mode, 0, 0, 0); //No need of bu
 }
 
-// Wake up a specific sensor
+// --- NEW: I2C Scanner Function ---
+//Because we use two I2C buses we can use this function to scan both buses separately without rewriting code again.
+void i2c_scanner(i2c_port_t port) {
+    printf("\n--- Scanning I2C Bus %d ---\n", port);
+    int devices_found = 0; //Counter for found devices
+    
+    // Standard I2C addresses are 1 to 127
+    for (uint8_t addr = 1; addr < 127; addr++) {// Loop through all possible I2C addresses. Use unit8_t to save memory.
+        // Try to write 0 bytes to the address to see if it ACKs
+        esp_err_t res = i2c_master_write_to_device(port, addr, NULL, 0, 100 / portTICK_PERIOD_MS); //Send a write bit to device to acknowledge if there is a sensor in this address
+        
+        if (res == ESP_OK) {
+            printf("Device found at address: 0x%02X\n", addr);
+            devices_found++;
+        }
+    }
+    
+    if (devices_found == 0) {
+        printf("No devices found on Bus %d. Check pull-ups and wiring!\n", port);
+    }
+    printf("---------------------------\n");
+}
+
+// --- Modified Read Function (Safe Version) ---
+esp_err_t mpu6050_read_all(i2c_port_t port, uint8_t addr, mpu_data_t *result) {
+    uint8_t reg = MPU6050_ACCEL_XOUT_H;
+    uint8_t buffer[14]; 
+
+    // Check return value!
+    esp_err_t ret = i2c_master_write_read_device(port, addr, &reg, 1, buffer, 14, 1000 / portTICK_PERIOD_MS);
+
+    if (ret == ESP_OK) {
+        result->acc_x  = (buffer[0] << 8) | buffer[1];
+        result->acc_y  = (buffer[2] << 8) | buffer[3];
+        result->acc_z  = (buffer[4] << 8) | buffer[5];
+        result->gyro_x = (buffer[8] << 8) | buffer[9];
+        result->gyro_y = (buffer[10] << 8) | buffer[11];
+        result->gyro_z = (buffer[12] << 8) | buffer[13];
+    } else {
+        // Clear data if read fails so we don't print garbage
+        memset(result, 0, sizeof(mpu_data_t));
+    }
+    return ret;
+}
+
 void mpu6050_wake(i2c_port_t port, uint8_t addr) {
     uint8_t data[2] = {MPU6050_PWR_MGMT_1, 0x00};
     i2c_master_write_to_device(port, addr, data, sizeof(data), 1000 / portTICK_PERIOD_MS);
 }
 
-// Read Accel and Gyro data
-void mpu6050_read_all(i2c_port_t port, uint8_t addr, mpu_data_t *result) {
-    uint8_t reg = MPU6050_ACCEL_XOUT_H;
-    uint8_t buffer[14]; // 6 Accel + 2 Temp + 6 Gyro
-
-    // Burst read 14 bytes
-    i2c_master_write_read_device(port, addr, &reg, 1, buffer, 14, 1000 / portTICK_PERIOD_MS);
-
-    // Parse Data (High byte << 8 | Low byte)
-    result->acc_x  = (buffer[0] << 8) | buffer[1];
-    result->acc_y  = (buffer[2] << 8) | buffer[3];
-    result->acc_z  = (buffer[4] << 8) | buffer[5];
-    // buffer[6] and [7] are temperature
-    result->gyro_x = (buffer[8] << 8) | buffer[9];
-    result->gyro_y = (buffer[10] << 8) | buffer[11];
-    result->gyro_z = (buffer[12] << 8) | buffer[13];
-}
-
 void app_main(void) {
     init_i2c_buses();
 
-    // Wake up all 3 sensors
-    mpu6050_wake(I2C_NUM_0, 0x68); // Bus 0, Sensor 1
-    mpu6050_wake(I2C_NUM_0, 0x69); // Bus 0, Sensor 2
-    mpu6050_wake(I2C_NUM_1, 0x68); // Bus 1, Sensor 3
-    
-    ESP_LOGI(TAG, "All 3 Sensors Initialized");
+    // 1. Run Scanner First
+    vTaskDelay(2000 / portTICK_PERIOD_MS); // Wait for power to stabilize
+    i2c_scanner(I2C_NUM_0);
+    i2c_scanner(I2C_NUM_1);
+
+    // 2. Initialize sensors
+    mpu6050_wake(I2C_NUM_0, 0x68);
+    mpu6050_wake(I2C_NUM_0, 0x69);
+    mpu6050_wake(I2C_NUM_1, 0x68);
 
     mpu_data_t s1, s2, s3;
 
     while (1) {
-        // Read Bus 0 Sensors
-        mpu6050_read_all(I2C_NUM_0, 0x68, &s1);
-        mpu6050_read_all(I2C_NUM_0, 0x69, &s2);
-        
-        // Read Bus 1 Sensor
-        mpu6050_read_all(I2C_NUM_1, 0x68, &s3);
+        esp_err_t ret1 = mpu6050_read_all(I2C_NUM_0, 0x68, &s1);
+        esp_err_t ret2 = mpu6050_read_all(I2C_NUM_0, 0x69, &s2);
+        esp_err_t ret3 = mpu6050_read_all(I2C_NUM_1, 0x68, &s3);
 
-        // Print Summary (Shortened for readability)
-        printf("--- DATA ---\n");
-        printf("S1 (Bus0 0x68): A[x:%d y:%d z:%d] G[x:%d y:%d z:%d]\n", 
-               s1.acc_x, s1.acc_y, s1.acc_z, s1.gyro_x, s1.gyro_y, s1.gyro_z);
-               
-        printf("S2 (Bus0 0x69): A[x:%d y:%d z:%d] G[x:%d y:%d z:%d]\n", 
-               s2.acc_x, s2.acc_y, s2.acc_z, s2.gyro_x, s2.gyro_y, s2.gyro_z);
-               
-        printf("S3 (Bus1 0x68): A[x:%d y:%d z:%d] G[x:%d y:%d z:%d]\n", 
-               s3.acc_x, s3.acc_y, s3.acc_z, s3.gyro_x, s3.gyro_y, s3.gyro_z);
+        printf("\n--- DATA ---\n");
+        if (ret1 == ESP_OK) printf("S1 (0x68): Ax:%d\n", s1.acc_x);
+        else printf("S1: DISCONNECTED\n");
 
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+        if (ret2 == ESP_OK) printf("S2 (0x69): Ax:%d\n", s2.acc_x);
+        else printf("S2: DISCONNECTED\n");
+
+        if (ret3 == ESP_OK) printf("S3 (Bus1): Ax:%d\n", s3.acc_x);
+        else printf("S3: DISCONNECTED\n");
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
